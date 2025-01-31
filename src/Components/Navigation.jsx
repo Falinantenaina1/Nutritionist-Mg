c1f8a77035d97d2bd46a9ebb1c38460cbe166627
Falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2025-01-31 17:11:41 +0300

    Ajout de la navigation

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,42 +1,126 @@
-import styled from "styled-components"
-import { ContainerWrapper } from "./ContainerWrapper"
-import { GlobalStyles } from "../Utils/GlobalStyle"
-import { NavLink } from "react-router-dom"
+import styled from "styled-components";
+import { ContainerWrapper } from "./ContainerWrapper";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import { useState } from "react";
 
 const Nav = styled.nav`
-    background-color: var(--dark-green-15);
-    color: var(--green-97);
-`
+  background-color: var(--dark-green-15);
+  color: var(--green-97);
+  border-bottom: 1px solid var(--green-90);
+`;
 
-const NavWrapper = styled(ContainerWrapper)`
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+`;
 
-`
+const BrandLogo = styled(Link)`
+  display: block;
+  width: 147px;
+  & > * {
+    display: block;
+    width: 100%;
+  }
+  @media screen and (min-width: 1440px){
+    width: 195px;
+  }
 
-const NavItem = styled.div`
+`;
 
-`
+const HamburgerMenu = styled.div`
+  cursor: pointer;
+  width: 34px;
+  height: 34px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-between;
+  padding: 8.5px 4.5px;
+  span {
+    width: 100%;
+    height: 3px;
+    border-radius: 25px;
+    background-color: var(--green-70);
+  }
 
-const Nav_link = styled(NavLink)`
+  span:last-child {
+    width: 50%;
+  }
 
-`
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
 
+const NavItem = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "isOpen",
+})`
+  display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+  flex-direction: column;
+  background: var(--dark-green-15);
+  width: 100%;
+  padding: 0 18px;
+  color: var(--green-97);
+  position: absolute;
+  top: 92px;
+  left: 0;
 
+  a {
+    margin: 4px 0;
+  }
 
+  a:hover,
+  a.active {
+    color: var(--green-70);
+  }
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    position: relative;
+    top: unset;
+    a {
+        margin-right: 20px;
+    }
+  }
+
+  @media screen and (min-width: 1920px) {
+    a {
+      font-size: 18px;
+      margin-right: 26px;
+    }
+  }
+`;
 
 export const Navigation = () => {
-
-    return <>
-        <GlobalStyles />
-        <Nav>
-            <NavWrapper>
-                <NavItem>
-                    <Nav_link to="">Home</Nav_link>
-                    <Nav_link to="team">Team</Nav_link>
-                    <Nav_link to="process">Process</Nav_link>
-                    <Nav_link to="pricing">Pricing</Nav_link>
-                    <Nav_link to="blog">Blog</Nav_link>
-                </NavItem>
-            </NavWrapper>
-        </Nav>
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Nav>
+        <ContainerWrapper>
+          <NavWrapper>
+            <BrandLogo to="">
+              <img src={Logo} alt="Nutritionist Logo" />
+            </BrandLogo>
+            <HamburgerMenu onClick={() => setIsOpen(!isOpen)}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </HamburgerMenu>
+            <NavItem isOpen={isOpen}>
+              <NavLink to="">Home</NavLink>
+              <NavLink to="team">Team</NavLink>
+              <NavLink to="process">Process</NavLink>
+              <NavLink to="pricing">Pricing</NavLink>
+              <NavLink to="blog">Blog</NavLink>
+            </NavItem>
+          </NavWrapper>
+        </ContainerWrapper>
+      </Nav>
     </>
-}
+  );
+};
--- src/Components/PageProvider.jsx
+++ src/Components/PageProvider.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 import { Navigation } from "./Navigation"
+import { GlobalStyles } from "../Utils/GlobalStyle"
 
 export const PageProvider = () => {
 
     return <>
+        <GlobalStyles />
         <Navigation />
         <Outlet />
     </>
